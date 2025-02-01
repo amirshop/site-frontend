1899322a0b6f5b0495a031f1783f15ac6af2689b
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-01 15:26:06 +0330

    Update user.model.ts

--- models/user.model.ts
+++ models/user.model.ts
@@ -2,43 +2,46 @@ import mongoose from "mongoose";
 
 const schema = mongoose.Schema;
 
-const userSchema = new schema({
-  id: {
-    type: mongoose.Schema.Types.ObjectId,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  name: {
-    first: {
+const userSchema = new schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    email: {
       type: String,
       required: true,
     },
-    last: {
+    username: {
       type: String,
       required: true,
     },
-  },
-  address: {
-    city: String,
-    street: String,
-    number: Number,
-    zipcode: String,
-    geolocation: {
-      lat: String,
-      long: String,
+    password: {
+      type: String,
+      required: true,
     },
+    name: {
+      first: {
+        type: String,
+        required: true,
+      },
+      last: {
+        type: String,
+        required: true,
+      },
+    },
+    address: {
+      city: String,
+      street: String,
+      number: Number,
+      zipcode: String,
+      geolocation: {
+        lat: String,
+        long: String,
+      },
+    },
+    phone: String,
   },
-  phone: String,
-});
+  { versionKey: false }
+);
 
 export default mongoose.model("user", userSchema);
