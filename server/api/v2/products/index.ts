e07dbfd15f88e747b5b1a4e460024d5315cea4b4
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-01 15:33:41 +0330

    Update index.ts

--- server/api/v2/products/index.ts
+++ server/api/v2/products/index.ts
@@ -1,13 +1,14 @@
-import { http } from "~/utils/http";
-
+import { connectDB } from "~/utils/db";
+import Product from "~/models/product.model";
 export default defineEventHandler(async (event) => {
+  await connectDB();
   try {
-    const { data } = await http.get("/products");
-    return data;
+    return await Product.find();
   } catch (error) {
+    const { message, statusCode } = error as Error;
     throw createError({
-      statusCode: 500,
-      statusMessage: "Failed to fetch products",
+      statusCode: statusCode || 500,
+      statusMessage: message || "Failed to Product",
     });
   }
 });
