7295ea718d8aeb425aa8de14b2e4bdc98601eba5
Amir Maghami <Maghami.A1987@gmail.com>
2025-02-04 00:03:47 +0330

    Update product.model.ts

--- server/services/product/product.model.ts
+++ server/services/product/product.model.ts
@@ -1,13 +1,24 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
 const ProductSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    title: { type: String, required: true },
     price: { type: Number, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+    slug: { type: String, unique: true },
     description: { type: String },
   },
   { timestamps: true }
 );
+
+ProductSchema.pre("validate", function (next) {
+  if (this.title && !this.slug) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  next();
+});
 
 export const ProductModel =
   mongoose.models.Product || mongoose.model("Product", ProductSchema);
